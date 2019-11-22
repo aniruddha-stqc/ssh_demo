2a69d84f119af11a6f270171cfd8be4cbcdebe02
Aniruddha - Kali PC <aniruddha2008@gmail.com>
2019-11-22 18:26:03 +0530

    Initial commit

---
+++ venv/bin/ssh_demo.py
@@ -0,0 +1,26 @@
+import paramiko
+import socket
+
+opts = paramiko.transport.Transport(socket.socket()).get_security_options()
+print(opts.ciphers)
+paramiko.Transport._preferred_ciphers = ('aes256-ctr', 'aes192-ctr', 'aes128-ctr')
+opts = paramiko.transport.Transport(socket.socket()).get_security_options()
+print(opts.ciphers)
+
+
+print(opts.kex)
+print(opts.digests)
+print(opts.key_types)
+print(opts.compression)
+
+print (paramiko.transport.Transport(socket.socket()).disabled_algorithms)
+
+
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('test.rebex.net',username='demo',password='password')
+(stdin, stdout, stderr) = client.exec_command("ls -al")
+for line in stdout.readlines():
+        print line
+client.close()
